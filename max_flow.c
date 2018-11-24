fd19d80cf3987db3a8b988957e66a00587dd8db4
LuisRizo <personal.luisrizo@gmail.com>
2018-11-24 13:45:17 -0500

    Fix Stack and its tests

--- max_flow.c
+++ max_flow.c
@@ -7,7 +7,7 @@
  * Class: Algorithms CS220 Fall 2018
  * Professor: Peter Brass
  * Function Signature:
- *  void maximum flow(int n, int s, int t, int *cap, int *flow); 
+ *  void maximum_flow(int n, int s, int t, int *cap, int *flow); 
  * Variables:
  * - n: the number of vertices of the graph,
  * - s: the start vertex,
@@ -33,24 +33,28 @@ StackClass* Stack_new(int n) {
     Stack->elements = (int *) malloc (n * sizeof(int));
     Stack->size     = 0;
     Stack->cap      = n;
+    return Stack;
 }
 
 void push(StackClass *Stack, int element) {
+    printf("Pushing value %d\n", element);
     if (Stack->size == Stack->cap) {
         printf("[Stack Error] - Stack is full to insert %d\n", element);
         exit(1);
     } else {
         *(Stack->elements + Stack->size) = element;
         Stack->size++;
+        printf("New Stack->size = %d\n", Stack->size);
     }
 }
 
 int pop(StackClass *Stack) {
-    int res, i;
+    int res;
+    printf("Stack size = %d\n", Stack->size);
     if (Stack->size > 0) {
-        res = *(Stack->elements);
+        res = *(Stack->elements + (Stack->size - 1));
         Stack->size--;
-        for (i = Stack->size - 1; i >= 0; i--) *(Stack->elements + i) = *(Stack->elements + i + 1);
+        printf("Pop result = %d\n", res);
         return res;
     } else {
         printf("[Stack Error] - Stack is empty to remove an element\n");
@@ -60,7 +64,7 @@ int pop(StackClass *Stack) {
 
 int peek(StackClass *Stack) {
     if (Stack->size > 0) {
-        return *(Stack->elements);
+        return *(Stack->elements + (Stack->size - 1));
     } else {
         printf("[Stack Error] - Stack is empty. Cannot peek\n");
         exit(1);
@@ -76,28 +80,30 @@ void maximum_flow(int n, int s, int t, int *cap, int *flow) {
 }
 
 int test_stack() {
-    int i;
-    StackClass *Stack = Stack_new(1000);
+    int i, size = 1000;
+    StackClass *Stack = Stack_new(size);
+    printf("Initialized Stack\n");
     push(Stack, 50);
+    printf("Inserted element Stack\n");
 
     // Assert pop in one element stack
     assert(pop(Stack) == 50);
     printf("Test 1\n");
 
     // Fill in Stack
-    for(i = 0; i < 1000; i++) {
+    for(i = 0; i < size; i++) {
         push(Stack, i * i);
     }
 
-    // Ensure peek only shows the first element;
-    for(i = 0; i < 1000; i++) {
-        assert(peek(Stack) == 0);
+    // Ensure peek only shows the last element;
+    for(i = 0; i < size; i++) {
+        assert(peek(Stack) == (size - 1) * (size - 1));
     }
     printf("Test 2 passed\n");
 
     // Ensure assert works for all the items
-    for(i = 0; i < 1000; i++) {
-        assert(pop(Stack) == i * i);
+    for(i = 0; i < size; i++) {
+        assert(pop(Stack) == (size - i - 1) * (size - i - 1));
     }
     printf("Test 3 passed\n");
     printf("All tests passed\n");
